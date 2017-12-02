9176fdf7c45874220a01ffa62694744b29583dce
tracer-ics2015 <tracer@njuics.org>
2017-12-02 21:15:48 +0800

    >  compile NEMU 141220000 yin Linux my-computer 4.10.0-40-generic #44~16.04.1-Ubuntu SMP Thu Nov 9 15:37:44 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:15:48 up 1 day,  5:13,  7 users,  load average: 0.01, 0.01, 0.00 ce82ea47e1edc1a138b309ff3c09837160df5c0d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -66,11 +66,13 @@ char mem_buf[100];
 static int cmd_dump_mem(char *args)
 {
 	int l = sprintf(mem_buf,"起始地址为：0x%8s\n",args);
+	printf("%d\n",atoi(args));
+
 	for(int i = 0;i<10;i++)
 	{
 		l+=sprintf(mem_buf+l,"%2x ",instr_fetch((unsigned)atoi(args)+i,1));
 	}
-	printf("%s",mem_buf);
+	printf("%s\n",mem_buf);
 	return 0;
 }
 
