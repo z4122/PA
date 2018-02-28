40983a48b2008597ba4246cc47ec436175662c9f
z4122 <412213484@qq.com>
2018-02-28 15:21:05 +0800

    >  compile NEMU 141220000 yin Linux my-computer 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:25:58 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:21:05 up 18:01,  8 users,  load average: 0.00, 0.00, 0.00 9b48d8bcbd0dcde0cbfe094996926f31dec7364

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -43,7 +43,7 @@ void reg_test() {
 }
 void init_eflags()
 {
-    cpu.eflags.n_1 = 0;
+    cpu.eflags.n_1 = 1;
     printf("eflags:%d\n",cpu.eflags.n_1);
     printf("eflags:%d\n",cpu.eflags.cf);
 }
